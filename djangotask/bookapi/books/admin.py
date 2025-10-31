567bb5aef7682e42aecb40676c3fee845d2500a4
Devasmita M <devasmitam5@gmail.com>
2025-10-31 11:08:40 +0530

    Changes

--- djangotask/bookapi/books/admin.py
+++ djangotask/bookapi/books/admin.py
@@ -3,4 +3,24 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author')
+    list_display = ('id', 'title', 'author', 'published_date')
+    list_display_links = ('id', 'title')
+    list_editable = ('author', 'published_date')
+    search_fields = ('title', 'author')
+    list_filter = ('author', 'published_date')
+    date_hierarchy = 'published_date'
+    ordering = ('-id',)
+    list_per_page = 25
+
+    # Show save buttons on top and bottom of the change form
+    save_on_top = True
+    actions = ['delete_selected']
+    actions_on_top = True
+    actions_on_bottom = True
+
+    # Field layout in the change form
+    fields = (
+        'title',
+        'author',
+        'published_date',
+    )
